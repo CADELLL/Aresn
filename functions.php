fca2a431c98ab90d176c7b23c1b9feda18983647
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-06 16:01:06 +0700

    done

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // connection
-$conn = mysqli_connect("localhost", "root", "", "epiz_28089065_spp");
+$conn = mysqli_connect("localhost", "root", "", "spp");
 
 // get url & folder
 $url = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
